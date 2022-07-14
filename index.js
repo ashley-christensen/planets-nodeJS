fdd9e84e77a4ee006e4a50c30cca508b0fa56299
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-07-13 17:20:40 -0700

    isHabitablePlanet

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+ return planet['koi_disposition'] === 'CONFIRMED';
+}
 
 fs.createReadStream('kepler_data.csv')
  .pipe(parse({
-   comment: '#',
-   columns: true,
+  comment: '#',
+  columns: true,
  }))
  .on('data', (data) => {
-  results.push(data);
+  if (isHabitablePlanet(data)) {
+   habitablePlanets.push(data);
+  }
+
  })
  .on('error', (err) => {
-  console.log(err)
+  console.log(err);
  })
  .on('end', () => {
-  console.log(results)
-  console.log('done processing file!!!')
- })
-// parse();
+  console.log(results);
+  console.log('done processing file!!!');
+ });
+
 

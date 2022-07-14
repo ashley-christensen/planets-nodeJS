3b2cd99939687b1d2727eddab8e0ad036d0016be
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-07-13 17:38:13 -0700

    flux and radius

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const { parse } = require('csv-parse');
 const fs = require('fs');
 const habitablePlanets = [];
 
+//! habitable if KOI disposition is confirmed
+//habitable if stellar flux has enough light 
+//upper limit for planet size
 function isHabitablePlanet(planet) {
- return planet['koi_disposition'] === 'CONFIRMED';
+ return planet['koi_disposition'] === 'CONFIRMED'
+  && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+  && planet['koi_prad'] < 1.6;
 }
+
+
 
 fs.createReadStream('kepler_data.csv')
  .pipe(parse({
